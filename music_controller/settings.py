c9c16b4c66920543fb273580e0fae88ba65867fa
Keith MacInnis <keith@keithmacinnis.com>
2021-08-09 23:32:00 -0700

    React inteegrated

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apiHandler.apps.ApihandlerConfig',
-    'rest_framework'
+    'rest_framework',
+    'front.apps.FrontConfig'
+
 ]
 
 MIDDLEWARE = [
--- music_controller/urls.py
+++ music_controller/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apiHandler.urls'))
+    path('api/', include('apiHandler.urls')),
+    path('', include('front.urls'))
 ]
